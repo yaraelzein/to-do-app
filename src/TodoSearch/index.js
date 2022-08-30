28d2c3cdccc6c486364216ac63ce2c57be8a865e
yaraelzein <yaraelzein2001@gmail.com>
2022-08-30 00:44:28 -0500

    creating a state to get the value of the input

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import './todoSearch.css'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
     return (
         <input 
             className="todoSearch" 
             placeholder="text"
+            value={searchValue}
             onChange={onSearchValueChange} 
         />
     );
